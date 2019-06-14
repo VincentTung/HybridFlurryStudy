1ab4e34606364a3e4a50af54b6579742a8166c17
dongxiangxiang <dongxiangxiang@cityhouse.cn>
2019-06-14 09:46:39 +0800

    change entity

--- HybridFlutter/lib_flutter/lib/entity/article.dart
+++ HybridFlutter/lib_flutter/lib/entity/article.dart
@@ -31,34 +31,4 @@ class Article {
   factory Article.fromJson(Map<String, dynamic> json) => _$ArticleFromJson(json);
   Map<String, dynamic> toJson() => _$ArticleToJson(this);
 
-//
-//"apkLink": "",
-//"author": "ixuea",
-//"chapterId": 328,
-//"chapterName": "网络&amp;文件下载",
-//"collect": false,
-//"courseId": 13,
-//"desc": "Android Downloader是一个开源的多线程，多任务下载框架",
-//"envelopePic": "https://wanandroid.com/blogimgs/ae7393e6-a5ba-4721-aeaa-7382c0da7d03.png",
-//"fresh": true,
-//"id": 8509,
-//"link": "http://www.wanandroid.com/blog/show/2585",
-//"niceDate": "16小时前",
-//"origin": "",
-//"prefix": "",
-//"projectLink": "https://github.com/ixuea/AndroidDownloader",
-//"publishTime": 1559045113000,
-//"superChapterId": 294,
-//"superChapterName": "开源项目主Tab",
-//"tags": [
-//{
-//"name": "项目",
-//"url": "/project/list/1?cid=328"
-//}
-//],
-//"title": "Android Downloader是一个开源的多线程，多任务下载框架",
-//"type": 0,
-//"userId": -1,
-//"visible": 1,
-//"zan": 0
 }
--- HybridFlutter/lib_flutter/lib/entity/community_info.dart
+++ HybridFlutter/lib_flutter/lib/entity/community_info.dart
@@ -1,6 +1,5 @@
 
 import 'package:json_annotation/json_annotation.dart';
-// user.g.dart 将在我们运行生成命令后自动生成
 part 'community_info.g.dart';
 @JsonSerializable()
 class CommunityInfo{
